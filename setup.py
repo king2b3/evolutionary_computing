04a15ffaf2140e742cf13a4dbacc59363962aeac
king2b3 <king2b3@mail.uc.edu>
2023-03-10 22:45:31 -0500

    empty files added, setup.py added
    resolves #4 mostly, still need some linting, and github actions, etc.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Evolutionary Computing',
+      version='0.1',
+      description='A python-based simple EC framework',
+      author='Bayley King',
+      author_email='bayleyking13@gmail.com',
+      packages=['numpy', 'matplotlib'],
+     )
