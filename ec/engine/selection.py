24f47d3320c28bb27f7c99c6660d33a2988d8b17
king2b3 <king2b3@mail.uc.edu>
2023-07-05 17:17:27 -0400

    fixed docstring form multi-line comment

--- ec/engine/selection.py
+++ ec/engine/selection.py
@@ -14,5 +14,5 @@ class Selection(abc.ABC):
 
     @abc.abstractmethod
     def __call__(self, pop) -> list:
-        ''' Returns the parent population
-        '''
+        """ Returns the parent population
+        """
